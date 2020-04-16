c445fd55752d81a45f77a8646e3babf39775cb0e
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-16 18:38:29 -0300

    definindo estilos para mina

--- src/components/Mine.js
+++ src/components/Mine.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+
+export default props => {
+  return (
+    <View styles={styles.container}>
+      <View style={styles.coreMine} />
+      <View style={styles.line} />
+    </View>
+  );
+};
