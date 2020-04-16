95d79e3e8fd83e19107c0e763740954ce6529f2a
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-16 18:03:10 -0300

    importando campo ao app

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Plataform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field />
       </View>
     );
   }
