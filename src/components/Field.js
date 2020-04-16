547ba4c7a19a8e9d3129f33416baf276079a2ce9
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-16 18:53:25 -0300

    adicionando propriedade exploded

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,11 +4,14 @@ import params from '../params';
 import Mine from './Mine';
 
 export default props => {
-  const {mined, opened, nearMines} = props;
+  const {mined, opened, nearMines, exploded} = props;
 
   const styleField = [styles.field];
   if (opened) {
     styleField.push(styles.opened);
+  }
+  if (exploded) {
+    styleField.push(styles.exploded);
   }
   if (styleField.length === 1) {
     styleField.push(styles.regular);
