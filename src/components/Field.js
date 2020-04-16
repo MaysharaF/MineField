6c2ace8065cc8b7bd89db5292f1da6f7b346e06c
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-16 18:18:03 -0300

    definindo estilo para o campo (nearMines)
    estilização dependendo da quantidade de minas próximo ao campo

--- src/components/Field.js
+++ src/components/Field.js
@@ -13,6 +13,22 @@ export default props => {
     styleField.push(styles.regular);
   }
 
+  let color = null;
+  if (nearMines > 0) {
+    if (nearMines == 1) {
+      color = '#2A28D7';
+    }
+    if (nearMines == 2) {
+      color = '#2B520F';
+    }
+    if (nearMines > 2 && nearMines < 6) {
+      color = '#F9060A';
+    }
+    if (nearMines >= 6) {
+      color = '#F221A9';
+    }
+  }
+
   return <View style={styleField} />;
 };
 
