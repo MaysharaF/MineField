4b5ae2e307ed84037832288e6e4f21f54629fc98
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-17 19:09:01 -0300

    remoção dos fields da view e import do field

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import params from './src/params';
-import Field from './src/components/Field';
 import MineField from './src/components/MineField';
 import {createMinedBoard} from './src/functions';
 
@@ -33,17 +32,6 @@ export default class App extends Component {
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened />
       </View>
     );
   }
