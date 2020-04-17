efecce5656040fe2e7b03cc2554a74ada9b6ad29
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-17 17:17:46 -0300

    definindo container e estilos para flag

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+
+export default props => {
+  return (
+    <View style={styles.container}>
+      <View styles={styles.flagpole} />
+      <View styles={styles.flag} />
+      <View styles={styles.base1} />
+      <View styles={styles.base2} />
+    </View>
+  );
+};
