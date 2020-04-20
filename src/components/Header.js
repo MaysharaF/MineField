11647298db6b5eaa417ad6591790b1285adf1c59
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-20 19:08:00 -0300

    definindo estilos para propriedades de flag no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Flag from './Flag';
+
+export default props => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.flagContainer}>
+        <TouchableOpacity onPress={props.onFlagPress} style={styles.flagButton}>
+          <Flag bigger />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={props.onNewGame}>
+        <Text style={styles.buttonLabel}>Novo Jogo</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
