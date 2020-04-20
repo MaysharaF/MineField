ef548f606449692c562788104cf07791a5fc9bd9
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-20 18:51:04 -0300

    passagem do onselect para minefield

--- src/components/MineField.js
+++ src/components/MineField.js
@@ -6,7 +6,12 @@ export default props => {
   const rows = props.board.map((row, r) => {
     const columns = row.map((field, c) => {
       return (
-        <Field {...field} key={c} onOpen={() => props.onOpenField(r, c)} />
+        <Field
+          {...field}
+          key={c}
+          onOpen={() => props.onOpenField(r, c)}
+          onSelect={e => props.onSelectField(r, c)}
+        />
       );
     });
     return (
